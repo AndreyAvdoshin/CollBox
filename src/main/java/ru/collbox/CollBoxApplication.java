6ab432cdb24e4789a13e8dc18c70811d1f48c500
AndreyAvdoshin <darkxbird@gmail.com>
2024-03-26 00:48:58 +0900

    feat: добавить скрипты создания таблиц в БД

---
+++ src/main/java/ru/collbox/CollBoxApplication.java
@@ -0,0 +1,13 @@
+package ru.collbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/collbox/CollBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.collbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
