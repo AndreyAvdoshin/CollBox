e4973676be25220487c07e8faf2ba733349a1e7e
AndreyAvdoshin <darkxbird@gmail.com>
2024-08-28 23:04:39 +0300

    feat: изменить AppContext

--- frontend/src/components/layout/AppContent.jsx
+++ frontend/src/components/layout/AppContent.jsx
@@ -4,13 +4,14 @@ const contentStyle = {
   width: "100%",
   minHeight: 120,
   color: "black",
+  padding: "20px",
   // backgroundColor: "#0958d9",
 };
 
 export default function AppContent() {
   return (
     <Layout.Content style={contentStyle}>
-      Content
+      <h2>Content</h2>
     </Layout.Content>
   );
 }
